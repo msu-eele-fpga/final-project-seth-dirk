71482f322f1b86b9c5528e0a8af0bc3fe2c64bfc
unknown <sethhoward2117@gmail.com>
2024-12-05 16:39:47 -0700

    Added avalon component for the buzzer in platform designer and instantiated the buzzer output in the top level file

--- hdl/buzzer/buzzer.vhd
+++ hdl/buzzer/buzzer.vhd
@@ -10,11 +10,11 @@ entity buzzer is
 		-- avalon memory-mapped slave interface
 		avs_read 		: in std_ulogic;
 		avs_write 		: in std_ulogic;
-		avs_address 	: in std_ulogic_vector(1 downto 0);
+		avs_address 	: in std_ulogic;
 		avs_readdata 	: out std_ulogic_vector(31 downto 0);
 		avs_writedata 	: in std_ulogic_vector(31 downto 0);
 		-- external I/O; export to top-level
-		buzzer_out 		: out std_ulogic;
+		buzzer_out 		: out std_ulogic
 		);
 end entity buzzer;
 
@@ -38,7 +38,10 @@ architecture buzzer_arch of buzzer is
 	end component pwm_controller;
 
 	signal period		  		: std_ulogic_vector(31 downto 0) := (26 => '1', others => '0'); --1 ms period
-	signal reg_pitch	 		: std_ulogic_vector(31 downto 0) := (others => '0'); --0% duty cycle
+	signal period_ms			: integer range 1 to 31 := 1;
+	signal period_fp			: integer range 2**26 to 31*(2**26):= 2**26;
+	signal reg_vol	 			: std_ulogic_vector(31 downto 0) := (others => '0'); --0% duty cycle
+	signal vol_fp				: integer range 0 to 2**19 := 1/2*(2**19);			  --50% duty cycle
 	signal reg_base_pitch	: std_ulogic_vector(31 downto 0) := (others => '0'); --0 Hz pitch
 	
 begin
@@ -48,7 +51,7 @@ begin
 		clk 				=> clk,
 		rst 				=> rst,
 		period 			=> unsigned(period),
-		duty_cycle 		=> std_logic_vector(reg_pitch(19 downto 0)),
+		duty_cycle 		=> std_logic_vector(reg_vol(19 downto 0)),
 		output		 	=> buzzer_out
 	);
 	
@@ -56,8 +59,8 @@ begin
 	begin
 		if rising_edge(clk) and avs_read = '1' then
 			case avs_address is
-				when "00"	=> avs_readdata	<= reg_pitch;
-				when "01" 	=> avs_readdata 	<= reg_base_pitch;
+				when '0'		=> avs_readdata	<= reg_vol;
+				when '1' 	=> avs_readdata 	<= reg_base_pitch;
 				when others => avs_readdata 	<= (others => '0');
 			end case;
 		end if;
@@ -66,12 +69,18 @@ begin
 	buzzer_register_write : process(clk, rst)
 	begin
 		if rst = '1' then		
-			reg_pitch	<= (others => '0');			--0% duty cycle
+			reg_vol				<= (others => '0');			--0% duty cycle
 			reg_base_pitch		<= (others => '0');	--0 Hz
 		elsif rising_edge(clk) and avs_write = '1' then
 			case avs_address is
-				when "00" 	=> reg_pitch 		<= avs_writedata;
-				when "01" 	=> reg_base_pitch	<= avs_writedata;
+				when '0' 	=> reg_vol <= avs_writedata;
+				when '1' 	=> 
+					--Fixed point operations to write buzzer period from frequency in Hz
+					reg_base_pitch	<= avs_writedata;
+						period_ms <= 1000/to_integer(unsigned(reg_base_pitch));
+						period_fp <= period_ms * 2**26;
+						period <= std_ulogic_vector(to_unsigned(period_fp, 32));
+						reg_vol <= std_ulogic_vector(to_unsigned(vol_fp, 32)); --50% duty cycle
 				when others => null;
 			end case;
 		end if;
